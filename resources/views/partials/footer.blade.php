f12e61fe9918ad085aaa546de45d3cc1d625dbf6
shikhar bhatnaggar <bhatnagar.shikhar@gmail.com>
2026-08-11 13:09:19 +0530

    Update footer text to reflect new company name

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="h-12 bg-white border-t flex items-center justify-center text-sm text-gray-500">
-    © {{ date('Y') }} Maharajah Enterprises
+    © {{ date('Y') }} Autoos360. All rights reserved.
 </footer>
