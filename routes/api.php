c91bfb8a74e04c3d507298109dd8c03382359970
shikhar bhatnaggar <bhatnaggar.shikhar@gmail.com>
2026-08-13 20:23:49 +0530

    Add test API route for Laravel

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,13 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\VehicleController;
 
+Route::get('/test-api', function () {
+    return response()->json([
+        'success' => true,
+        'message' => 'Laravel API route is working',
+    ]);
+});
+
 Route::get(
     '/tenants/{tenant_uuid}/branches/{branch_id}/vehicles',
     [VehicleController::class, 'index']
