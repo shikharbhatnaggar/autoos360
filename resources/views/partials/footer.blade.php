7fba5cc4817229297c7c5a31b492e4f82021f586
shikhar bhatnagar <bhatnagar.shikhar@gmail.com>
2026-08-13 11:20:33 +0530

    document upload feature

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="h-12 bg-white border-t flex items-center justify-center text-sm text-gray-500">
-    © {{ date('Y') }} Autoos360. All rights reserved.
+    © {{ date('Y') }} AutoOS360. All rights reserved.
 </footer>
